e0d48a29b603f81f09bbe8026dd516007c7a0c79
Hiong Kai Han <kaihanhiong@gmail.com>
2022-07-05 22:02:47 +0800

    Add skeleton code

---
+++ app.js
@@ -0,0 +1,7 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a + b;
+
+let displayValue = 0;
+//const operate = function(operator, a, b) => operator()
